cedf9ddc22080178a7c57aa64a1e39086777b5db
ranik20 <ranik20@gmail.com>
2026-04-26 21:31:30 +0300

    java gamres

--- JavaScript/exercises/exercises.js
+++ JavaScript/exercises/exercises.js
@@ -1,13 +1,13 @@
-console.log("Hello, World!");
-console.log('Hello, World!');
-console.log( 10 + 10 );
-console.log( 5 * 5 );
-console.log( 20 - 5 );
-console.log( 100 / 10 );
-console.log(5 + 2 * 7);
-console.log("hello" + " Everyone"); 
-console.log("The result of 5 + 2 * 7 is: " + (5 + 2 * 7));
-console.log("The result of (5 + 2) * 7 is: " + ((5 + 2) * 7));
+// console.log("Hello, World!");
+// console.log('Hello, World!');
+// console.log( 10 + 10 );
+// console.log( 5 * 5 );
+// console.log( 20 - 5 );
+// console.log( 100 / 10 );
+// console.log(5 + 2 * 7);
+// console.log("hello" + " Everyone"); 
+// console.log("The result of 5 + 2 * 7 is: " + (5 + 2 * 7));
+// console.log("The result of (5 + 2) * 7 is: " + ((5 + 2) * 7));
 // let name = "Alice";console.log("Hello, " + name + "!");
 // let age = 30; 
 // console.log(name + " is " + age + " years old.");
@@ -21,9 +21,25 @@ console.log("The result of (5 + 2) * 7 is: " + ((5 + 2) * 7));
 //  const currentYear = 2026;
 //  const age2 = currentYear - birthYear;
 //  console.log("The age is: " + age2);
-let name = "dani";
-let age = 25;
-const country = "ISRAEL";
+// let name = "dani";
+// let age = 25;
+// const country = "ISRAEL";
 
-console.log(name + " is " + age + " years old and lives in " + country + ".");
-console.log(`Hey, I am ${name} and I am ${age} years old, and I live in ${country}.`);
+// console.log(name + " is " + age + " years old and lives in " + country + ".");
+// console.log(`Hey, I am ${name} and I am ${age} years old, and I live in ${country}.`);
+let age = 20;
+let hasTicket = true;
+
+if (age > 18 && hasTicket === true) {
+  console.log("you can enter the concert.");
+} else {
+  console.log("sorry, you cannot enter the concert.");
+}   
+const randumNumber = Math.random();
+if (randumNumber >= 0    && randumNumber < 1/3)  {
+  console.log("The number is between 0 and 1/3.");
+} else if (randumNumber >= 1/3 && randumNumber < 2/3) {
+  console.log("The number is between 1/3 and 2/3.");
+} else {
+  console.log("The number is between 2/3 and 1.");
+}   
