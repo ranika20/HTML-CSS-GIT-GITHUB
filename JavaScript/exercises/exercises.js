1519858b26fdd4169ce641ae688adb5162fde0d0
ranik20 <ranik20@gmail.com>
2026-04-30 18:46:22 +0300

    games

--- JavaScript/exercises/exercises.js
+++ JavaScript/exercises/exercises.js
@@ -27,19 +27,97 @@
 
 // console.log(name + " is " + age + " years old and lives in " + country + ".");
 // console.log(`Hey, I am ${name} and I am ${age} years old, and I live in ${country}.`);
-let age = 20;
-let hasTicket = true;
+// let age = 20;
+// let hasTicket = true;
 
-if (age > 18 && hasTicket === true) {
-  console.log("you can enter the concert.");
-} else {
-  console.log("sorry, you cannot enter the concert.");
-}   
-const randumNumber = Math.random();
-if (randumNumber >= 0    && randumNumber < 1/3)  {
-  console.log("The number is between 0 and 1/3.");
-} else if (randumNumber >= 1/3 && randumNumber < 2/3) {
-  console.log("The number is between 1/3 and 2/3.");
-} else {
-  console.log("The number is between 2/3 and 1.");
-}   
+// if (age > 18 && hasTicket === true) {
+//   console.log("you can enter the concert.");
+// } else {
+//   console.log("sorry, you cannot enter the concert.");
+// }   
+// const randumNumber = Math.random();
+// if (randumNumber >= 0    && randumNumber < 1/3)  {
+//   console.log("The number is between 0 and 1/3.");
+// } else if (randumNumber >= 1/3 && randumNumber < 2/3) {
+//   console.log("The number is between 1/3 and 2/3.");
+// } else {
+//   console.log("The number is between 2/3 and 1.");
+// }   
+
+// function name() 
+// {
+//  let firstName = "dani";
+//  let lastName = "levi";
+//  console.log("My name is " + firstName + " " + lastName + ".");   
+
+// }
+// name();
+// function givenumber() {
+//   return 5;
+// }
+// const result = givenumber();
+// console.log(result);
+//  <!-- <script>
+//       const emojies = ['😎', '🎉', '🏆', '🔥', '🥳'];
+      
+//       function playGame(player) {
+//         const rand = Math.random();
+//         let comp = '';
+//         if (rand < 1 / 3) {
+//           comp = 'rock';
+//         } else if (rand < 2 / 3) {
+//           comp = 'paper';
+//         } else {
+//           comp = 'scissors';
+//         }
+
+//         let res = '';
+//         if (player === 'rock') {
+//           res = comp === 'rock' ? 'tie' : comp === 'paper' ? 'you lose' : 'you win';
+//         } else if (player === 'paper') {
+//           res = comp === 'rock' ? 'you win' : comp === 'paper' ? 'tie' : 'you lose';
+//         } else {
+//           res = comp === 'rock' ? 'you lose' : comp === 'paper' ? 'you win' : 'tie';
+//         }
+
+//         let emo = '';
+//         if (res === 'you win') {
+//           emo = ' ' + emojies[Math.floor(Math.random() * emojies.length)];
+//         }
+//         document.getElementById('result').textContent =
+//           'Computer picked ' + comp + '. ' + res + emo;
+//       }
+//     </script>
+
+//     <button onclick="playGame('rock')">Rock</button>
+//     <button onclick="playGame('paper')">Paper</button>
+//     <button onclick="playGame('scissors')">Scissors</button>
+//     <script>
+//       function computermove() 
+//       {
+//         const randnumber= Math.random();
+//         if (randnumber < 1 / 3)
+//          {
+//           return 'rock';
+//         } 
+//         else if (randnumber < 2 / 3) 
+//         {
+//           return 'paper';
+//         } 
+//         else 
+//         {
+//           return 'scissors';
+//         }
+//       }
+//     </script>
+//     <script>
+//       function playGame(player) {
+//         const comp = computermove();  
+//         let res = '';
+//         if (player === 'rock') {
+//           res = comp === 'rock' ? 'tie' : comp === 'paper' ? 'you lose' : 'you win';
+//         } else if (player === 'paper') {      
+//           res = comp === 'rock' ? 'you win' : comp === 'paper' ? 'tie' : 'you lose';
+//         } else {
+//           res = comp === 'rock' ? 'you lose' : comp === 'paper' ? 'you win' : 'tie';
+//         }
